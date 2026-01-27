26bcc477376d03b2455f7d8b3339a84705f26824
Зубайру Гасанов <A-Pond>
2026-01-27 07:36:32 +0300

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -77,36 +77,115 @@
 
 // export default App;
 
-import './App.module.css';
+// import './App.module.css';
+// import { useState } from "react";
+
+// const App = () => {
+
+//   const [count, setCount] = useState(0);
+//   const [open, setOpen] = useState(true);
+
+//   function setLet() {
+//     setOpen((prev) => !prev)
+//   }
+//   return (
+//     <>
+//       <button onClick={setLet}>Start</button>
+//       {open || (
+//         <>
+
+//           <p>React & Vite = {count >= 2 ? 'Application' : ''}</p>
+//           <p>Count is {count}</p>
+//           <div>
+//             <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
+//             <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
+//             <button onClick={() => setCount(0)}>Reset</button>
+//           </div>
+//         </>
+//       )}
+//     </>
+//   )
+// }
+
+// export default App;
+
 import { useState } from "react";
+import img from '../public/img1.png'
+import style from './App.module.css'
 
-const App = () => {
+const data = [
+  {
+    id: 1,
+    title: 'card 1',
+    description: 'desk card 1',
+    date: 'March 01 2001',
+    image: '/img1.png',
+    tags: '#tag1 #tag2 #tag3',
+    archived: false,
+  },
+  {
+    id: 2,
+    title: 'card 2',
+    description: 'desk card 2',
+    date: 'March 02 2002',
+    image: './',
+    tags: '#tag1 #tag2 #tag3',
+    archived: false,
+  },
+  {
+    id: 3,
+    title: 'card 3',
+    description: 'desk card 3',
+    date: 'March 03 2003',
+    image: './',
+    tags: '#tag1 #tag2 #tag3',
+    archived: false,
+  },
+  {
+    id: 4,
+    title: 'card 4',
+    description: 'desk card 4',
+    date: 'March 04 2004',
+    image: './',
+    tags: '#tag1 #tag2 #tag3',
+    archived: false,
+  },
+  {
+    id: 5,
+    title: 'card 5',
+    description: 'desk card 5',
+    date: 'March 05 2005',
+    image: '',
+    tags: '#tag1 #tag2 #tag3',
+    archived: false,
+  },
+]
 
-  const [count, setCount] = useState(0);
-  const [open, setOpen] = useState(true);
+const dataSpred = { ...data };
 
-  function setLet() {
-    setOpen((prev) => !prev)
-  }
+const App = (props) => {
   return (
     <>
-      <button onClick={setLet}>Start</button>
-      {open || (
-        <>
-
-          <p>React & Vite = {count >= 2 ? 'Application' : ''}</p>
-          <p>Count is {count}</p>
-          <div>
-            <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
-            <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
-            <button onClick={() => setCount(0)}>Reset</button>
-          </div>
-        </>
-      )}
+      <Card title={data[0].title} />
+      {data.map((item) => {
+        <Card object={item} />
+      })}
     </>
   )
 }
 
-
+const Card = (props) => {
+  return (
+      <div className={style.container}>
+        <div className={style.header}>
+          <img src={img} />
+        </div>
+        <div className={style.body}>
+          <p>{props.object.title}</p>
+          <p>{data[0].description}</p>
+        </div>
+      </div>
+  )
+}
 
 export default App;
